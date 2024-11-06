c04266183a55733c8b9960ddaa41eb67da8f176e
Budsakorn Wannaphuek <wannapk.budsakorn@gmail.com>
2024-11-06 08:12:53 +0000

    contosol

---
+++ server.js
@@ -0,0 +1,16 @@
+'use strict'
+
+const express = require('express');
+
+const PORT = 8000;
+const HOST = 'localhost';
+
+const app=express();
+
+app.get('/',(req,res)=> {
+    res.send('Hello World! - 1.0.0');
+});
+
+app.listen(PORT,HOST,()=> {
+    console.log(`Running on http://${HOST}:${PORT}`);
+});
